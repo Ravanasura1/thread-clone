a7b99119bdf0ddb39621440f03c183dd52eaa09d
Shantanu dev upadhyay <shantanudevupadhyay@gmail.com>
2024-05-19 08:32:00 +0530

    changes in update user

--- fronted/src/pages/UserPage.jsx
+++ fronted/src/pages/UserPage.jsx
@@ -1,67 +1,193 @@
-import { useEffect, useState } from "react";
-import UserHeader from "../components/UserHeader";
-import { useParams } from "react-router-dom";
-import useShowToast from "../hooks/useShowToast";
-import { Flex, Spinner } from "@chakra-ui/react";
-import Post from "../components/Post";
-import useGetUserProfile from "../hooks/useGetUserProfile";
+"use client";
+
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+  Avatar,
+  Center,
+} from "@chakra-ui/react";
+
+import { useRef, useState } from "react";
 import { useRecoilState } from "recoil";
-import postsAtom from "../atoms/postsAtom";
+import userAtom from "../atoms/userAtom";
+import usePreviewImg from "../hooks/usePreviewImg";
+import useShowToast from "../hooks/useShowToast";
 
-const UserPage = () => {
-	const { user, loading } = useGetUserProfile();
-	const { username } = useParams();
-	const showToast = useShowToast();
-	const [posts, setPosts] = useRecoilState(postsAtom);
-	const [fetchingPosts, setFetchingPosts] = useState(true);
+export default function UpdateProfilePage() {
+  const [user, setUser] = useRecoilState(userAtom);
+  const [updating, setUpdating] = useState(false);
+  const [inputs, setInputs] = useState({
+    name: user.name,
+    username: user.username,
+    password: user.password,
+    email: user.email,
+    bio: user.bio,
+  });
 
-	useEffect(() => {
-		const getPosts = async () => {
-			if (!user) return;
-			setFetchingPosts(true);
-			try {
-				const res = await fetch(`/api/post/user/${username}`);
-				const data = await res.json();
-				setPosts(data);
-			} catch (error) {
-				showToast("Error", error.message, "error");
-				setPosts([]);
-			} finally {
-				setFetchingPosts(false);
-			}
-		};
+  const fileRef = useRef(null);
+  const showToast = useShowToast();
 
-		getPosts();
-	}, [username, showToast, setPosts, user]);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prevInputs) => ({ ...prevInputs, [name]: value }));
+  };
 
-	if (!user && loading) {
-		return (
-			<Flex justifyContent={"center"}>
-				<Spinner size={"xl"} />
-			</Flex>
-		);
-	}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (updating) return;
+    setUpdating(true);
+    try {
+      const res = await fetch(`/api/users/update/${user._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...inputs, profilePic: imageUrl }),
+      });
 
-	
+      const data = await res.json();
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
 
-	if (!user && !loading) return <h1>User not found</h1>;
+      showToast("Success", "Profile updated successfully", "success");
 
-	return (
-		<>
-			<UserHeader user={user} />
+      setUser(data);
+      localStorage.setItem("user-threads", JSON.stringify(data));
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    } finally {
+      setUpdating(false);
+    }
+  };
 
-			{!fetchingPosts && posts.length === 0 && <h1>User has not posts.</h1>}
-			{fetchingPosts && (
-				<Flex justifyContent={"center"} my={12}>
-					<Spinner size={"xl"} />
-				</Flex>
-			)}
+  const { handleImageChange, imageUrl } = usePreviewImg();
 
-			{posts.map((post) => (
-				<Post key={post._id} post={post} postedBy={post.postedBy} />
-			))}
-		</>
-	);
-};
-
-export default UserPage;
+  return (
+    <form onSubmit={handleSubmit}>
+      <Flex align={"center"} justify={"center"} my={6}>
+        <Stack
+          spacing={4}
+          w={"full"}
+          maxW={"md"}
+          bg={useColorModeValue("white", "gray.800")}
+          rounded={"xl"}
+          boxShadow={"lg"}
+          p={6}
+        >
+          <Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
+            User Profile Edit
+          </Heading>
+          <FormControl id="profilePic">
+            <Stack direction={["column", "row"]} spacing={6}>
+              <Center>
+                <Avatar
+                  size="xl"
+                  boxShadow={"md"}
+                  src={imageUrl || user.profilePic}
+                />
+              </Center>
+              <Center w="full">
+                <Button w="full" onClick={() => fileRef.current.click()}>
+                  Change Avatar
+                </Button>
+                <input
+                  type="file"
+                  hidden
+                  ref={fileRef}
+                  onChange={handleImageChange}
+                />
+              </Center>
+            </Stack>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Full name</FormLabel>
+            <Input
+              name="name"
+              placeholder="Full Name"
+              _placeholder={{ color: "gray.500" }}
+              type="text"
+              value={inputs.name || ""}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>User name</FormLabel>
+            <Input
+              name="username"
+              placeholder="UserName"
+              _placeholder={{ color: "gray.500" }}
+              type="text"
+              value={inputs.username || ""}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Email address</FormLabel>
+            <Input
+              name="email"
+              placeholder="your-email@example.com"
+              _placeholder={{ color: "gray.500" }}
+              type="email"
+              value={inputs.email || ""}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Bio</FormLabel>
+            <Input
+              name="bio"
+              placeholder="Your bio"
+              _placeholder={{ color: "gray.500" }}
+              type="text"
+              value={inputs.bio || ""}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Password</FormLabel>
+            <Input
+              name="password"
+              placeholder="password"
+              _placeholder={{ color: "gray.500" }}
+              type="password"
+              value={inputs.password || ""}
+              onChange={handleInputChange}
+            />
+          </FormControl>
+          <Stack spacing={6} direction={["column", "row"]}>
+            <Button
+              bg={"red.400"}
+              color={"white"}
+              w="full"
+              _hover={{
+                bg: "red.500",
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              bg={"green.400"}
+              color={"white"}
+              w="full"
+              _hover={{
+                bg: "green.500",
+              }}
+              type="submit"
+              isLoading={updating}
+            >
+              Submit
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+    </form>
+  );
+}
